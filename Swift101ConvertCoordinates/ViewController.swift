3873e77f4c2de8b9400bec51fa01b2f653c65569
David Seek <davidseek1986@gmail.com>
2018-09-05 10:07:30 -0700

    Initial Commit

---
+++ Swift101ConvertCoordinates/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift101ConvertCoordinates
+//
+//  Created by David Seek on 9/5/18.
+//  Copyright © 2018 David Seek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
